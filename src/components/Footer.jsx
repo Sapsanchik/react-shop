b5f469a116691ad60f1b8c51a399672f0d81d43d
Sapsanchik <99867200+Sapsanchik@users.noreply.github.com>
2023-02-09 20:27:31 +0700

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a className="grey-text text-lighten-4 right" href="https://github.com/Sapsanchik/react-shop" target='_blank' rel="noreferrer">
             Repo
           </a>
         </div>
